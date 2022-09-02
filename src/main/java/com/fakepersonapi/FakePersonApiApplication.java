adf8488c6bbc394b16a6d24e4b42aeea0786040b
Ori Bibas <oribibas6@gmail.com>
2022-09-01 23:10:49 -0400

    initial maven project structure

---
+++ src/main/java/com/fakepersonapi/FakePersonApiApplication.java
@@ -0,0 +1,13 @@
+package com.fakepersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakePersonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakePersonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fakepersonapi/FakePersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakepersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakePersonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
