8213091263f45eb8ee009a5ebd563403f22d1049
Ori Bibas <oribibas6@gmail.com>
2022-09-15 23:38:11 -0400

    Added functionality to retrieve one person at endpoint /person

--- src/main/java/com/fakepersonapi/Controllers/PersonController.java
+++ src/main/java/com/fakepersonapi/Controllers/PersonController.java
@@ -1,7 +1,20 @@
 package com.fakepersonapi.Controllers;
 
+import com.fakepersonapi.Entities.Person;
+import com.fakepersonapi.Services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping("/person")
+    public Person getPerson() {
+        return personService.createPerson();
+    }
+
 }
