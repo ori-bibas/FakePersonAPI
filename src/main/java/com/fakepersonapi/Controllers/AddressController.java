ac779061b2c59ed9f1fcaf4bc4f6be814e2cc9bb
Ori Bibas <oribibas6@gmail.com>
2022-09-19 22:51:51 -0400

    Added "/address" endpoint to return an address

--- src/main/java/com/fakepersonapi/Controllers/AddressController.java
+++ src/main/java/com/fakepersonapi/Controllers/AddressController.java
@@ -1,7 +1,20 @@
 package com.fakepersonapi.Controllers;
 
+import com.fakepersonapi.Entities.Address;
+import com.fakepersonapi.Services.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping("/address")
+    public Address address() {
+        return addressService.createAddress();
+    }
+
 }
