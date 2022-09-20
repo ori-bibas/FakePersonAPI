f870b5ce2f77fa9f13a0bb36d794d382b855de8d
Ori Bibas <oribibas6@gmail.com>
2022-09-19 22:51:07 -0400

    Added method to create random address

--- src/main/java/com/fakepersonapi/Services/AddressService.java
+++ src/main/java/com/fakepersonapi/Services/AddressService.java
@@ -1,7 +1,33 @@
 package com.fakepersonapi.Services;
 
+import com.fakepersonapi.Entities.Address;
+import com.fakepersonapi.Repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public Address createAddress() {
+        long count = addressRepository.count();
+
+        Random random = new Random();
+        int randomStreetAddressPk = random.nextInt((int) count) + 1;
+        int randomCityAndStatePk = random.nextInt((int) count) + 1;
+        int randomZipCodePk = random.nextInt((int) count) + 1;
+
+        Address address = new Address();
+        address.setStreetAddress(addressRepository.getReferenceById((long) randomStreetAddressPk).getStreetAddress());
+        address.setCity(addressRepository.getReferenceById((long) randomCityAndStatePk).getCity());
+        address.setState(addressRepository.getReferenceById((long) randomCityAndStatePk).getState());
+        address.setZipCode(addressRepository.getReferenceById((long) randomZipCodePk).getZipCode());
+
+        return address;
+    }
+
 }
